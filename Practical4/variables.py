bc3149211f9ef7a09cd2f8216cfbef2426ad3627
rare-gold-sun <Zhuanz@macAir.local>
2026-03-11 14:50:08 +0800

    Scotland population

---
+++ Practical4/variables.py
@@ -0,0 +1,13 @@
+a = 5.08
+b = 5.33
+c = 5.55
+d = b - a
+e = c - b
+Compare = d - e
+if Compare > 0:
+	print("d is larger" )
+elif Compare < 0:
+       print("e is larger")
+else:
+	 print("d equals to e")
+#population growth decelerating in Scotland
